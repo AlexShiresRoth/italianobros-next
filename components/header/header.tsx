0cd5701f859346e9073152146f349ff352a852c1
Alex Roth <alexroth96@gmail.com>
2024-08-03 18:18:40 -0400

    Start header, need to have sub menu. Also need mobile design

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,12 +1,12 @@
-import { fetchGraphQL } from '@/contentful/api';
-import { headerQuery } from '@/contentful/gql-queries/components/header/header.query';
-import { UnknownComponent } from '@/types/component';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import Nav from '../navigation/nav';
-import { NavigationData } from '@/types/nav';
-import { cn } from '@/lib/utils';
+import { fetchGraphQL } from "@/contentful/api";
+import { headerQuery } from "@/contentful/gql-queries/components/header/header.query";
+import { UnknownComponent } from "@/types/component";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Nav from "../navigation/nav";
+import { NavigationData } from "@/types/nav";
+import { cn } from "@/lib/utils";
 
 type Props = {
   data?: UnknownComponent;
@@ -23,8 +23,8 @@ export interface Header {
     title: string;
   };
   title: string;
-  actionItemsCollection: NavigationData['actionItemsCollection'];
-  navItemsCollection: NavigationData['navItemsCollection'];
+  actionItemsCollection: NavigationData["actionItemsCollection"];
+  navItemsCollection: NavigationData["navItemsCollection"];
 }
 
 export interface HeaderResponseData {
@@ -41,7 +41,7 @@ async function getHeaderData(id: string) {
 
     return header;
   } catch (error) {
-    console.error('Error fetching header data:', error);
+    console.error("Error fetching header data:", error);
     return null;
   }
 }
@@ -56,23 +56,26 @@ export default async function Header({ data, slug }: Props) {
   }
 
   return (
-    <div className="flex flex-col w-full fixed top-0 left-0 z-40 md:relative">
-      <span className="w-full bg-gradient-to-l from-rose-400 via-fuchsia-500 to-indigo-700 h-2 block"></span>
+    <div className='flex flex-col w-full fixed top-0 left-0 z-40 md:relative'>
       <header
-        className="w-full flex flex-col items-center md:py-4 bg-white border-b border-b-stone-100 dark:border-b-gray-900 dark:bg-black"
-        data-component-type="header"
+        className='w-full flex flex-col items-center md:py-2 bg-white border-b border-b-stone-100'
+        data-component-type='header'
       >
-        <div className="flex flex-row w-full px-8 lg:px-0 md:w-11/12 lg:w-3/4 gap-8 items-center">
-          <div className="flex flex-col md:flex-row md:flex-wrap items-center py-4">
-            <HeaderLogo logo={header.logo} title={header.title} />
-          </div>
+        <div className='grid grid-cols-3 w-full px-8 lg:px-0 md:w-11/12 lg:w-3/4 gap-8 items-center justify-between'>
           <Nav
             actionItemsCollection={header.actionItemsCollection}
             navItemsCollection={header.navItemsCollection}
             slug={slug}
           />
+          <div className='flex flex-col md:flex-row md:flex-wrap items-center justify-center'>
+            <HeaderLogo logo={header.logo} title={header.title} />
+          </div>
+          <div className='flex items-center justify-end'>
+            <button className='text-grey-500 dark:text-gray-500 border border-primary py-3 px-10 font-light text-sm font-garamond uppercase'>
+              Contact Us
+            </button>
+          </div>
         </div>
-        <div className="w-11/12 mx-4 mt-2 flex gap-4 items-center justify-between md:hidden"></div>
       </header>
     </div>
   );
@@ -81,11 +84,13 @@ export default async function Header({ data, slug }: Props) {
 const LogoClassNamesBase = `relative z-10 text-2xl md:text-2xl text-black before:h-3 before:rounded-full before:w-[105%] before:block before:content-[' '] before:bg-indigo-500 before:absolute before:skew-y-1 before:-left-[7px] before:bottom-[2px] before:-z-10`;
 const LogoClassNamesDarkMode = `dark:text-white`;
 
-const HeaderLogo = ({ logo, title }: Pick<Header, 'logo' | 'title'>) => (
-  <Link href={'/'} className="flex items-center">
-    {logo && <Image src={logo.url} alt={logo.title} height={60} width={60} />}
-    <h2 className={cn([LogoClassNamesBase, LogoClassNamesDarkMode])}>
-      {title}
-    </h2>
+const HeaderLogo = ({ logo, title }: Pick<Header, "logo" | "title">) => (
+  <Link href={"/"} className='flex items-center'>
+    {logo && <Image src={logo.url} alt={logo.title} width={120} height={60} />}
+    {!logo && (
+      <h2 className={cn([LogoClassNamesBase, LogoClassNamesDarkMode])}>
+        {title}
+      </h2>
+    )}
   </Link>
 );
--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 // import NavItems from './nav-items';
-import Link from 'next/link';
-import classNames from 'classnames';
-import { NavigationData } from '@/types/nav';
+import Link from "next/link";
+import classNames from "classnames";
+import { NavigationData } from "@/types/nav";
 
 type NavProps = NavigationData & { slug?: string };
 
@@ -13,28 +13,31 @@ const Nav = async ({
 }: NavProps) => {
   return (
     <>
-      <nav className="flex justify-center">
-        <div className="w-full flex items-center  mt-4 py-2">
-          <div className="w-full flex items-center gap-4 justify-between text-black dark:text-white">
+      <nav className='flex justify-center'>
+        <div className='w-full flex items-center py-2'>
+          <div className='w-full flex items-center gap-4 justify-between text-black dark:text-white'>
             {navItemsCollection.items.map((item) => (
               <div key={item.sys.id}>
-                {item.menuItemsCollection.items.map((menuItem) => (
-                  <Link
-                    href={menuItem.groupLink.slug ?? null}
-                    key={menuItem.sys.id}
-                    className={classNames(
-                      'transition-all hover:border-b-indigo-500 hover:text-indigo-500',
-                      {
-                        'border-black border-b ':
-                          slug === menuItem.groupLink.slug,
-                        'border-transparent border-b ':
-                          slug !== menuItem.groupLink.slug,
-                      }
-                    )}
-                  >
-                    {menuItem.groupName}
-                  </Link>
-                ))}
+                {item.menuItemsCollection.items.map((menuItem) => {
+                  console.log(menuItem);
+                  return (
+                    <Link
+                      href={menuItem.groupLink.slug ?? null}
+                      key={menuItem.sys.id}
+                      className={classNames(
+                        "transition-all hover:border-b-indigo-500 hover:text-indigo-500",
+                        {
+                          "border-black border-b ":
+                            slug === menuItem.groupLink.slug,
+                          "border-transparent border-b ":
+                            slug !== menuItem.groupLink.slug,
+                        }
+                      )}
+                    >
+                      {menuItem.groupName}
+                    </Link>
+                  );
+                })}
               </div>
             ))}
           </div>
