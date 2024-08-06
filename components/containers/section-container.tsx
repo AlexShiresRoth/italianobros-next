5e3bc8e147d89b00fe669a844cabd7b571edede7
Alex Roth <alexroth96@gmail.com>
2024-08-06 17:05:23 -0400

    add cta to featured section

--- components/containers/section-container.tsx
+++ components/containers/section-container.tsx
@@ -4,7 +4,7 @@ export default function SectionContainer({
   children: React.ReactNode;
 }) {
   return (
-    <section className='flex flex-col items-center justify-center w-full md:w-11/12  px-8 md:px-0 py-10 md:py-24'>
+    <section className='flex flex-col items-center justify-center w-full md:w-11/12  px-8 md:px-0 py-10 md:py-16'>
       {children}
     </section>
   );
